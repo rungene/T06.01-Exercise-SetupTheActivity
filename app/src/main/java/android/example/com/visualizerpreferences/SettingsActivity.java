f942195d74d700e1e95531d7275591fa564ec51a
rungene <rungenesamuel@gmail.com>
2018-02-08 17:35:13 +0300

    working on the settings activity

--- app/src/main/java/android/example/com/visualizerpreferences/SettingsActivity.java
+++ app/src/main/java/android/example/com/visualizerpreferences/SettingsActivity.java
@@ -1,7 +1,10 @@
 package android.example.com.visualizerpreferences;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -9,5 +12,35 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        ActionBar actionBar = this.getSupportActionBar();
+
+        //set the action bar back button to look like an up button
+
+        if (actionBar !=null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+
+    }
+/*    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //use AppCompatActivity's method getMenuInflater to get a handle on the menu inflater
+        MenuInflater inflater = getMenuInflater();
+        //use the inflater's inflate method to inflate our menu layout to this menu
+        inflater.inflate(R.menu.visualizer_menu,menu);
+        //return true so that the menu is displayed in the toolbar
+        return true;
+    }*/
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        //When the home buttons pressed ,take the user back to the VisualizerActivity
+
+        if (id==R.id.home) {
+            NavUtils.navigateUpFromSameTask(this);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
